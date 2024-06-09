3e4ac90eedde2fbfe326a1173920979ceec48cf4
Nikel Stanislav <162141430+Nikel-Stanislav@users.noreply.github.com>
2024-06-09 21:28:32 +0300

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+const profile = {
+  //Метод changeUsername(newName) повинен приймати рядок(нове ім’я) в параметр newName та змінювати значення властивості
+  //username на нове.Нічого не повертає.
+  // Метод updatePlayTime(hours) повинен приймати число(кількість годин) у параметр hours та збільшити на нього значення
+  // властивості playTime.Нічого не повертає.
+  // Метод getInfo() має повертати рядок формату<Username> has<amount> active hours!, де<Username> — це ім’я профілю,
+  // а<amount> — кількість ігрових годин.
+  username: "Jacob",
+  playTime: 300,
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function (hours) {
+    this.playTime = hours;
+  },
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
